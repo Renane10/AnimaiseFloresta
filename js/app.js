0179bb2bcf228237a6af6c9d488078606f966ec7
renan <renan.gsilva@aluno.faculdadeimpacta.com.br>
2022-11-06 10:28:02 -0300

    Otimizando projeto

--- js/app.js
+++ js/app.js
@@ -1,27 +1,24 @@
-$(document).ready(function(){
-	var classActive = 'active';
+$('[data-group]').each(function() {
 
-	$('.animais .tab-menu a').first().addClass(classActive);
-	$('.animais .item').first().addClass(classActive);
+	var $allTarget = $(this).find('[data-target]'),
+			$allClick = $(this).find('[data-click]'),
+			activeClass = 'active';
+	
+	console.log($allClick);
 
-	$('.animais .tab-menu a').click(function(e){
+	$allClick.first().addClass(activeClass);
+	$allTarget.first().addClass(activeClass);
+
+	$allClick.click(function(e) {
 		e.preventDefault();
-		var itemId = $(this).attr('href');
 
-		$('.animais .tab-menu a, .animais  .item').removeClass(classActive);
-		$(this).addClass(classActive);
-		$(itemId).addClass(classActive);
-	});
+		var id = $(this).data('click'),
+				$target = $('[data-target="' + id + '"]');
 
-	$('.florestas .tab-menu a').first().addClass(classActive);
-	$('.florestas .item').first().addClass(classActive);
+		$allClick.removeClass(activeClass)
+		$allTarget.removeClass(activeClass);
 
-	$('.florestas .tab-menu a').click(function(e){
-		e.preventDefault();
-		var itemId = $(this).attr('href');
-
-		$('.florestas .tab-menu a, .florestas  .item').removeClass(classActive);
-		$(this).addClass(classActive);
-		$(itemId).addClass(classActive);
+		$target.addClass(activeClass);
+		$(this).addClass(activeClass);
 	});
 });
